e43f1cc8c6581d63edb9012c8a08dd95da516865
Crazypsy92 <Y2734034804@163.com>
2025-12-13 11:19:55 +0800

    延时函数驱动

--- MyApplication/MyApplication.h
+++ MyApplication/MyApplication.h
@@ -19,6 +19,15 @@
 
 #include "My_Delay.h"
 
+#define DEBUG 0
+#if DEBUG
+// 启用调试时，定义DEBUG_PRINTF为printf
+#define DEBUG_PRINTF(fmt, ...) printf(fmt, ##__VA_ARGS__)
+#else
+// 禁用调试时，定义为空宏（不产生任何代码）
+#define DEBUG_PRINTF(fmt, ...) do {} while(0)
+#endif
+
 #endif
 /********************************************************
   End Of File
